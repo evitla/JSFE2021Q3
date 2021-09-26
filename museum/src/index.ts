c7b599d0c37957f920e734b36658d18d2305a0ed
evitla <s.dinmuhamed@gmail.com>
2021-09-26 20:35:32 +0600

    feat: add welcome section

--- museum/src/index.ts
+++ museum/src/index.ts
@@ -1,4 +1,7 @@
 import header from './components/header/header';
+import main from './components/main/main';
+
 import './styles/style.scss';
 
 document.body.appendChild(header);
+document.body.appendChild(main);
