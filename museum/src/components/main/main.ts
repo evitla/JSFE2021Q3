ac445cb8776a38565d7ee30c0652aeb490d823c1
evitla <s.dinmuhamed@gmail.com>
2021-09-27 11:29:59 +0600

    refactor: make some refactoring

--- museum/src/components/main/main.ts
+++ museum/src/components/main/main.ts
@@ -1,8 +1,12 @@
 import baseComponent from '../baseComponent';
 import welcomeSection from './welcome/welcome';
+import virtualSection from './virtual/virtual';
+
+import './main.scss';
 
 const main = baseComponent('main');
 
 main.appendChild(welcomeSection);
+main.appendChild(virtualSection);
 
 export default main;
