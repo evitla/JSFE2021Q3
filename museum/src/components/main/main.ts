c1c572219a5904c12f85af773a3d594a71cadd69
evitla <s.dinmuhamed@gmail.com>
2021-09-28 12:42:29 +0600

    feat: add gallery section

--- museum/src/components/main/main.ts
+++ museum/src/components/main/main.ts
@@ -3,6 +3,7 @@ import welcomeSection from './welcome/welcome';
 import virtualSection from './virtual/virtual';
 import exploreSection from './explore/explore';
 import videoSection from './video/video';
+import gallerySection from './gallery/gallery';
 
 import './main.scss';
 
@@ -12,5 +13,6 @@ main.appendChild(welcomeSection);
 main.appendChild(virtualSection);
 main.appendChild(exploreSection);
 main.appendChild(videoSection);
+main.appendChild(gallerySection);
 
 export default main;
