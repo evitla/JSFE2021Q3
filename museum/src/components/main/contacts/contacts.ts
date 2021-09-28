a40c6b67fffd74fba8fbdf8bf4fdcf438415251e
evitla <s.dinmuhamed@gmail.com>
2021-09-28 14:34:34 +0600

    feat: add parallax and contact section

---
+++ museum/src/components/main/contacts/contacts.ts
@@ -0,0 +1,25 @@
+import baseComponent from '../../baseComponent';
+
+import imgSrc from '../../../assets/images/map.png';
+
+import './contacts.scss';
+
+const contactsSection = baseComponent('section', ['white-bg']);
+const contactsWrapper = baseComponent('div', ['wrapper', 'contacts-wrapper']);
+contactsWrapper.innerHTML = `
+  <h2 class="section-title title-with-line">Contacts</h2>
+  <div class="contacts-container">
+    <div class="contacts">
+      <h4>Palais Royal</h4>
+      <h4>Musee du Louvre</h4>
+      <p>address: 75001 Paris, France</p>
+      <p>phone: +33(0) 1 40 20 50 50</p>
+      <p>mail: info@louvre.fr</p>
+    </div>
+    <img src=${imgSrc} alt="">
+  </div>
+`;
+
+contactsSection.appendChild(contactsWrapper);
+
+export default contactsSection;
--- museum/src/components/main/main.ts
+++ museum/src/components/main/main.ts
@@ -5,10 +5,13 @@ import exploreSection from './explore/explore';
 import videoSection from './video/video';
 import gallerySection from './gallery/gallery';
 import ticketsSection from './tickets/tickets';
+import contactsSection from './contacts/contacts';
 
 import './main.scss';
 
 const main = baseComponent('main');
+
+const parallax = baseComponent('div', ['parallax']);
 
 main.appendChild(welcomeSection);
 main.appendChild(virtualSection);
@@ -16,5 +19,7 @@ main.appendChild(exploreSection);
 main.appendChild(videoSection);
 main.appendChild(gallerySection);
 main.appendChild(ticketsSection);
+main.appendChild(parallax);
+main.appendChild(contactsSection);
 
 export default main;
