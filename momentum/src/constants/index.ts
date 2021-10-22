d340129b0dc98002b8f325c05bd69991914d86b1
evitla <s.dinmuhamed@gmail.com>
2021-10-22 21:55:50 +0600

    feat: add playList

---
+++ momentum/src/constants/index.ts
@@ -0,0 +1,29 @@
+import audio1 from '../assets/sounds/Aqua Caelestis.mp3';
+import audio2 from '../assets/sounds/River Flows In You.mp3';
+import audio3 from '../assets/sounds/Ennio Morricone.mp3';
+import audio4 from '../assets/sounds/Summer Wind.mp3';
+
+const playList = [
+  {
+    title: 'Aqua Caelestis',
+    src: audio1,
+    duration: '00:58',
+  },
+  {
+    title: 'River Flows In You',
+    src: audio2,
+    duration: '03:50',
+  },
+  {
+    title: 'Ennio Morricone',
+    src: audio3,
+    duration: '01:37',
+  },
+  {
+    title: 'Summer Wind',
+    src: audio4,
+    duration: '01:50',
+  },
+];
+
+export default playList;
