b8b93020a1f723326408038f81aab2290169e356
evitla <s.dinmuhamed@gmail.com>
2021-10-22 21:58:53 +0600

    feat: split player into components

--- momentum/src/components/Player/index.ts
+++ momentum/src/components/Player/index.ts
@@ -1,16 +1,26 @@
 import baseComponent from '../baseComponent';
+import unorderedList from '../unorderedList';
+
+import playList from '../../constants';
 
 import './style.scss';
 
 const player = baseComponent('div', ['player']);
 
-player.innerHTML = `
-  <div class="player-controls">
-    <button class="play-prev player-icon"></button>
-    <button class="play player-icon"></button>
-    <button class="play-next player-icon"></button>
-  </div>
-  <ul class="play-list"></ul>
-`;
+const playListContainer = unorderedList(
+  playList.map((p) => p.title),
+  ['play-list']
+);
+
+const playerControls = baseComponent('div', ['player-controls']);
+const playPrevButton = baseComponent('button', ['play-prev', 'player-icon']);
+const playButton = baseComponent('button', ['play', 'player-icon']);
+const playNextButton = baseComponent('button', ['play-next', 'player-icon']);
+
+playerControls.appendChild(playPrevButton);
+playerControls.appendChild(playButton);
+playerControls.appendChild(playNextButton);
+player.appendChild(playerControls);
+player.appendChild(playListContainer);
 
 export default player;
