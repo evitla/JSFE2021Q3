e870e657d1cffc175edc3c814d252fb5bbeb6fe3
evitla <s.dinmuhamed@gmail.com>
2021-10-22 22:01:24 +0600

    feat: create playAudio function

--- momentum/src/components/Player/index.ts
+++ momentum/src/components/Player/index.ts
@@ -17,6 +17,31 @@ const playPrevButton = baseComponent('button', ['play-prev', 'player-icon']);
 const playButton = baseComponent('button', ['play', 'player-icon']);
 const playNextButton = baseComponent('button', ['play-next', 'player-icon']);
 
+const audio = new Audio();
+
+let audioCurrentTime = 0;
+let isPlay = false;
+
+const playAudio = (songId: number, canPause = true) => {
+  if (isPlay && canPause) {
+    audio.pause();
+    audioCurrentTime = audio.currentTime;
+    isPlay = false;
+    return;
+  }
+
+  Array.from(playListContainer.children, (li, index) => {
+    li.classList.remove('active');
+    if (index === songId) li.classList.add('active');
+    return null;
+  });
+
+  audio.src = playList[songId].src;
+  audio.currentTime = audioCurrentTime;
+  audio.play();
+  isPlay = true;
+};
+
 playerControls.appendChild(playPrevButton);
 playerControls.appendChild(playButton);
 playerControls.appendChild(playNextButton);
