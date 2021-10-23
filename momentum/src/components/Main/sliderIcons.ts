3f893dafd9e19a40c91ef9b746e94e3ac220a49b
evitla <s.dinmuhamed@gmail.com>
2021-10-24 00:25:27 +0600

    feat: implement slider to change main background image

--- momentum/src/components/Main/sliderIcons.ts
+++ momentum/src/components/Main/sliderIcons.ts
@@ -1,8 +1,27 @@
 import baseComponent from '../baseComponent';
+import { getRandomNumber, setBackground } from '../utils';
 
 const sliderIcons = baseComponent('div', ['slider-icons']);
 const prevButton = baseComponent('button', ['slide-prev', 'slider-icon']);
 const nextButton = baseComponent('button', ['slide-next', 'slider-icon']);
+
+const [minBgNum, maxBgNum] = [1, 20];
+
+let bgNum = getRandomNumber(minBgNum, maxBgNum);
+setBackground(document.body, bgNum);
+
+const getPrevSlide = () => {
+  bgNum = bgNum === minBgNum ? maxBgNum : bgNum - 1;
+  setBackground(document.body, bgNum);
+};
+
+const getNextSlide = () => {
+  bgNum = bgNum === maxBgNum ? minBgNum : bgNum + 1;
+  setBackground(document.body, bgNum);
+};
+
+prevButton.onclick = getPrevSlide;
+nextButton.onclick = getNextSlide;
 
 sliderIcons.appendChild(prevButton);
 sliderIcons.appendChild(nextButton);
---
+++ momentum/src/components/utils/index.ts
@@ -0,0 +1,24 @@
+const getTimeOfDay = () => {
+  const timesOfDay = ['night', 'morning', 'afternoon', 'evening'];
+  const hours = new Date().getHours();
+
+  return timesOfDay[Math.floor(hours / 6)];
+};
+
+export const getRandomNumber = (min: number, max: number): number =>
+  Math.floor(Math.random() * max) + min;
+
+export const setBackground = (element: HTMLElement, bgNum: number): void => {
+  const baseUrl =
+    'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images';
+  const timeOfDay = getTimeOfDay();
+  const bgNumToString = bgNum.toString().padStart(2, '0');
+
+  const imageUrl = `${baseUrl}/${timeOfDay}/${bgNumToString}.jpg`;
+
+  const img = new Image();
+  img.src = imageUrl;
+  img.onload = () => {
+    element.style.backgroundImage = `url(${imageUrl})`;
+  };
+};
