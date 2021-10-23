e981acb3216b1363039847ce0df60c0d969e9168
evitla <s.dinmuhamed@gmail.com>
2021-10-23 12:56:11 +0600

    feat: add getWeather function

--- momentum/src/components/Weather/index.ts
+++ momentum/src/components/Weather/index.ts
@@ -1,3 +1,4 @@
+import { WEATHER_URL } from '../../constants';
 import baseComponent from '../baseComponent';
 
 import './style.scss';
@@ -14,6 +15,21 @@ const temperature = baseComponent('span', ['temperature']);
 const weatherDescription = baseComponent('span', ['weather-description']);
 const wind = baseComponent('div', ['wind']);
 const humidity = baseComponent('div', ['humidity']);
+
+const getWeather = async () => {
+  const data = await (await fetch(WEATHER_URL)).json();
+
+  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+
+  cityName.value = data.name;
+  temperature.textContent = `${Math.round(data.main.temp)}°C`;
+  weatherDescription.textContent = data.weather[0].description;
+  wind.textContent = `Wind speed: ${Math.round(data.wind.speed)} m/s`;
+  humidity.textContent = `Humidity: ${data.main.humidity}%`;
+};
+
+getWeather();
 
 descriptionContainer.appendChild(temperature);
 descriptionContainer.appendChild(weatherDescription);
