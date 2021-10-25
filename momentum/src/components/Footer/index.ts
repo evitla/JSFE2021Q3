f46fd7fb5cd17dc61b51504cc07d30fd0a26ecdd
evitla <s.dinmuhamed@gmail.com>
2021-10-25 13:50:28 +0600

    refactor: split footer into components

--- momentum/src/components/Footer/index.ts
+++ momentum/src/components/Footer/index.ts
@@ -4,12 +4,12 @@ import './style.scss';
 
 const footer = baseComponent('footer', ['footer']);
 
-footer.innerHTML = `
-  <button class="change-quote"></button>
-  <div>
-    <div class="quote"></div>
-    <div class="author"></div>
-  </div>
-`;
+const changeQuoteButton = baseComponent('button', ['change-quote']);
+const quoteContainer = baseComponent('div');
+const quote = baseComponent('div', ['quote']);
+const author = baseComponent('div', ['author']);
+
+quoteContainer.append(quote, author);
+footer.append(changeQuoteButton, quoteContainer);
 
 export default footer;
