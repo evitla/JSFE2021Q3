472873bf7713c0bfca9a3c22fd70fe3565a6033c
evitla <s.dinmuhamed@gmail.com>
2021-10-25 14:38:00 +0600

    feat: implement quotes widget

--- momentum/src/components/Footer/index.ts
+++ momentum/src/components/Footer/index.ts
@@ -1,4 +1,5 @@
 import baseComponent from '../baseComponent';
+import { getRandomNumber } from '../utils';
 
 import './style.scss';
 
@@ -6,10 +7,37 @@ const footer = baseComponent('footer', ['footer']);
 
 const changeQuoteButton = baseComponent('button', ['change-quote']);
 const quoteContainer = baseComponent('div');
-const quote = baseComponent('div', ['quote']);
+const quoteText = baseComponent('div', ['quote']);
 const author = baseComponent('div', ['author']);
 
-quoteContainer.append(quote, author);
+const getQuotes = async (lang: 'en' | 'ru') => {
+  const url = './quotes.json';
+  const quotes: QuotesProps[] = (await (await fetch(url)).json())[lang];
+  return quotes;
+};
+
+const writeQuote = (quote: QuotesProps) => {
+  quoteText.textContent = quote.text;
+  author.textContent = quote.author;
+};
+
+getQuotes('en').then((quotes) => {
+  let quoteId = getRandomNumber(0, quotes.length);
+
+  writeQuote(quotes[quoteId]);
+
+  changeQuoteButton.onclick = () => {
+    quoteId = getRandomNumber(0, quotes.length);
+    writeQuote(quotes[quoteId]);
+  };
+});
+
+quoteContainer.append(quoteText, author);
 footer.append(changeQuoteButton, quoteContainer);
 
 export default footer;
+
+interface QuotesProps {
+  text: string;
+  author: string;
+}
