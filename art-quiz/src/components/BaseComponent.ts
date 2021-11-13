a53f7003e82a8b3a9beabc14a66785977a1931e4
evitla <s.dinmuhamed@gmail.com>
2021-11-13 19:41:38 +0600

    feat: add base component

---
+++ art-quiz/src/components/BaseComponent.ts
@@ -0,0 +1,13 @@
+class BaseComponent {
+  readonly element: HTMLElement;
+
+  constructor(
+    tagName: keyof HTMLElementTagNameMap = 'div',
+    styles: string[] = []
+  ) {
+    this.element = document.createElement(tagName);
+    this.element.classList.add(...styles);
+  }
+}
+
+export default BaseComponent;
