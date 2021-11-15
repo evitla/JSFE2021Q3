df1959c412ffb24b89f2af9d2f9142a6124f8747
evitla <s.dinmuhamed@gmail.com>
2021-11-15 12:42:24 +0600

    refactor: move h1 tag to logo

--- art-quiz/src/components/Logo/index.ts
+++ art-quiz/src/components/Logo/index.ts
@@ -2,9 +2,18 @@ import BaseComponent from '../BaseComponent';
 
 import './style.scss';
 
+import logoImage from '../../assets/images/logo.svg';
+
 class Logo extends BaseComponent {
   constructor(styles: string[] = []) {
     super('div', ['logo', ...styles]);
+
+    this.element.innerHTML = `
+      <a href="#/">
+        <img class="logo-image" src=${logoImage} alt="logo">
+        <h1 class="logo-title">Art Quiz</h1>
+      </a>
+    `;
   }
 }
 
--- art-quiz/src/index.ts
+++ art-quiz/src/index.ts
@@ -5,8 +5,4 @@ import './styles/style.scss';
 const main = new Main();
 main.render();
 
-document.body.innerHTML = `
-  <h1 class="visually-hidden">Art-Quiz</h1>
-`;
-
 document.body.append(main.element);
