fcedf3d563f0df90f7d87a56598b451459bc4a05
evitla <s.dinmuhamed@gmail.com>
2021-11-19 09:25:23 +0600

    refactor: move parseRequestURL method to utils

--- art-quiz/src/router/index.ts
+++ art-quiz/src/router/index.ts
@@ -1,18 +1,5 @@
 import BasePage from '../pages/BasePage';
-
-const parseRequestURL = () => {
-  const url = window.location.hash.slice(1).toLowerCase() || '/';
-
-  const r = url.split('/');
-
-  const request = {
-    resource: r[1],
-    id: r[2],
-    verb: r[3],
-  };
-
-  return request;
-};
+import { parseRequestURL } from '../utils';
 
 class Router {
   constructor(readonly routes: { [key: string]: BasePage }) {}
--- art-quiz/src/utils/index.ts
+++ art-quiz/src/utils/index.ts
@@ -4,3 +4,21 @@ export const getImageURL = (imageNum: number): string => {
 
   return `${baseUrl}/${imageNum}.jpg`;
 };
+
+export const parseRequestURL = (): {
+  resource: string;
+  id: string;
+  verb: string;
+} => {
+  const url = window.location.hash.slice(1).toLowerCase() || '/';
+
+  const r = url.split('/');
+
+  const request = {
+    resource: r[1],
+    id: r[2],
+    verb: r[3],
+  };
+
+  return request;
+};
