707252162f7a43aed5789b1b744eb9acab72f272
evitla <s.dinmuhamed@gmail.com>
2021-11-22 17:45:34 +0600

    refactor: layout

--- art-quiz/src/layout/index.ts
+++ art-quiz/src/layout/index.ts
@@ -1,19 +1,29 @@
 class Layout {
   readonly element = document.body;
 
+  private main = document.createElement('main');
+
+  private mainWrapper = document.createElement('div');
+
+  private footer = document.createElement('footer');
+
+  private footerWrapper = document.createElement('div');
+
+  constructor() {
+    this.mainWrapper.className = 'wrapper';
+    this.footerWrapper.className = 'wrapper';
+  }
+
   async render(children: HTMLElement): Promise<void> {
-    this.element.innerHTML = `
-      <main>
-        <div class="wrapper">
-          ${children.outerHTML}
-        </div>
-      </main>
-      <footer>
-        <div class="wrapper">
-          <h2>Footer</h2>
-        </div>
-      </footer>
-    `;
+    this.main.append(this.mainWrapper);
+    this.mainWrapper.append(children);
+    this.footer.append(this.footerWrapper);
+    this.footerWrapper.innerHTML = `<h2>Footer</h2>`;
+    this.element.append(this.main, this.footer);
+  }
+
+  async clear(): Promise<void> {
+    this.mainWrapper.innerHTML = '';
   }
 }
 
