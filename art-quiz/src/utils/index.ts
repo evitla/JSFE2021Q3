914f16865c8691b684fcef771209d737dcd153ca
evitla <s.dinmuhamed@gmail.com>
2021-11-22 22:16:51 +0600

    feat: getRandomNumbers function

--- art-quiz/src/utils/index.ts
+++ art-quiz/src/utils/index.ts
@@ -1,8 +1,16 @@
+import { IImageProps } from '../models';
+
 export const getImageURL = (imageNum: number): string => {
   const baseUrl =
     'https://raw.githubusercontent.com/evitla/image-data/master/img';
 
   return `${baseUrl}/${imageNum}.jpg`;
+};
+
+export const getImages = async (url: string): Promise<IImageProps[]> => {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json.images;
 };
 
 export const parseRequestURL = (): {
@@ -22,3 +30,14 @@ export const parseRequestURL = (): {
 
   return request;
 };
+
+export const getRandomNumber = (min: number, max: number): number =>
+  Math.floor(Math.random() * max) + min;
+
+export const getRandomNumbers = (
+  len: number,
+  min: number,
+  max: number
+): number[] =>
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  Array.from({ length: len }, (_) => getRandomNumber(min, max));
