64bf74d810725d228aab6d8c14cbb46a877fa6f3
evitla <s.dinmuhamed@gmail.com>
2021-11-24 11:46:05 +0600

    feat: add withTitle argument

--- art-quiz/src/components/Logo/index.ts
+++ art-quiz/src/components/Logo/index.ts
@@ -5,13 +5,13 @@ import './style.scss';
 import logoImage from '../../assets/images/logo.svg';
 
 class Logo extends BaseComponent {
-  constructor(styles: string[] = []) {
+  constructor(styles: string[] = [], withTitle = true) {
     super('div', ['logo', ...styles]);
 
     this.element.innerHTML = `
       <a href="#/">
         <img class="logo-image" src=${logoImage} alt="logo">
-        <h1 class="logo-title">Art Quiz</h1>
+        ${withTitle ? '<h1 class="logo-title">Art Quiz</h1>' : ''}
       </a>
     `;
   }
