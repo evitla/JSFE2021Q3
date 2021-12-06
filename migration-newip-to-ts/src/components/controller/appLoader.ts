25dcc023e20e1be94d30e68efa623449d5a6d26c
evitla <s.dinmuhamed@gmail.com>
2021-12-06 09:15:42 +0600

    feat: add api key

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '7cc6142d0a2c4fc49f2c835b24c3118c',
         });
     }
 }
