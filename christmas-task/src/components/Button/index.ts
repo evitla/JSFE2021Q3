9093325d55bb1d4d230176006cd8a7eccb2eecd2
evitla <s.dinmuhamed@gmail.com>
2021-12-14 17:39:10 +0600

    feat: add href to Button component

--- christmas-task/src/components/Button/index.ts
+++ christmas-task/src/components/Button/index.ts
@@ -7,17 +7,36 @@ class Button extends BaseComponent {
 
   content: string;
 
-  constructor(parentNode: HTMLElement, content: string, styles: string[] = []) {
+  href: string;
+
+  constructor(
+    parentNode: HTMLElement,
+    content: string,
+    styles: string[] = [],
+    href = ''
+  ) {
     super('button', ['button', ...styles]);
 
     this.parentNode = parentNode;
     this.content = content;
+    this.href = href;
   }
 
   render(): void {
     this.element.innerHTML = this.content;
 
     this.parentNode.appendChild(this.element);
+  }
+
+  afterRender(callback: () => void): void {
+    if (this.href) {
+      this.element.onclick = () => {
+        const link = document.createElement('a');
+        link.href = this.href;
+        link.click();
+        callback();
+      };
+    }
   }
 }
 
