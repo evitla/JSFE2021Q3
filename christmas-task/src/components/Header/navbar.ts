b5d62569b578fc1b710fd9c144ac970cdb81f45d
evitla <s.dinmuhamed@gmail.com>
2021-12-14 17:24:55 +0600

    refactor: navbar

--- christmas-task/src/components/Header/navbar.ts
+++ christmas-task/src/components/Header/navbar.ts
@@ -3,35 +3,44 @@ import BaseComponent from '../BaseComponent';
 class Navbar extends BaseComponent {
   parentNode: HTMLElement;
 
+  toys = document.createElement('a');
+
+  tree = document.createElement('a');
+
   constructor(parentNode: HTMLElement) {
     super('nav', ['navbar']);
+
+    this.toys.href = '#/toys';
+    this.tree.href = '#/tree';
 
     this.parentNode = parentNode;
   }
 
   render(): void {
-    this.element.innerHTML = `
-      <ul>
-        <li><a id="toys" href="#/toys"><h2>Toys</h2></a></li>
-        <li><a id="tree" href="#/tree"><h2>Tree</h2></a></li>
-      </ul>
-    `;
+    const ul = document.createElement('ul');
+    const item1 = document.createElement('li');
+    const item2 = document.createElement('li');
 
+    this.toys.innerHTML = '<h2>Toys</h2>';
+    this.tree.innerHTML = '<h2>Tree</h2>';
+
+    item1.appendChild(this.toys);
+    item2.appendChild(this.tree);
+    ul.append(item1, item2);
+
+    this.element.appendChild(ul);
     this.parentNode.appendChild(this.element);
   }
 
   afterRender(): void {
-    const toys: HTMLElement = this.element.querySelector('#toys');
-    const tree: HTMLElement = this.element.querySelector('#tree');
-
-    toys.onclick = () => {
-      toys.classList.add('active');
-      tree.className = '';
+    this.toys.onclick = () => {
+      this.toys.classList.add('active');
+      this.tree.className = '';
     };
 
-    tree.onclick = () => {
-      tree.classList.add('active');
-      toys.className = '';
+    this.tree.onclick = () => {
+      this.tree.classList.add('active');
+      this.toys.className = '';
     };
   }
 }
