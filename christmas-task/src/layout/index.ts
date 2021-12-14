2c9688635fad79d7e3bec71f7cf32f4610e337bd
evitla <s.dinmuhamed@gmail.com>
2021-12-14 17:12:44 +0600

    feat: add layout

---
+++ christmas-task/src/layout/index.ts
@@ -0,0 +1,31 @@
+import { Header } from '../components';
+
+class Layout {
+  readonly element = document.body;
+
+  private header = new Header(this.element);
+
+  private main = document.createElement('main');
+
+  private mainWrapper = document.createElement('div');
+
+  constructor() {
+    this.mainWrapper.className = 'wrapper';
+
+    this.header.render();
+    this.header.afterRender();
+
+    this.main.append(this.mainWrapper);
+    this.element.appendChild(this.main);
+  }
+
+  async render(children: HTMLElement): Promise<void> {
+    this.mainWrapper.append(children);
+  }
+
+  async clear(): Promise<void> {
+    this.mainWrapper.innerHTML = '';
+  }
+}
+
+export default Layout;
