3e3f8f5c382eb9e312ceb9180ae0de55d195d11a
evitla <s.dinmuhamed@gmail.com>
2021-12-15 15:33:22 +0600

    feat: add toy cards to toys page with fake filtering and sorting

--- christmas-task/src/pages/Toys/index.ts
+++ christmas-task/src/pages/Toys/index.ts
@@ -1,26 +1,49 @@
-import BaseComponent from '../../components/BaseComponent';
 import BasePage from '../BasePage';
+import Button from '../../components/Button';
+import ToyCards from '../../components/ToyCards';
+import { IToyProps } from '../../types';
 
 import './style.scss';
 
 class Toys extends BasePage {
-  private content = new BaseComponent('div', ['toys-content']);
+  filterButton = new Button(this.element, 'Filter', ['button-primary']);
 
-  constructor() {
+  sortButton = new Button(this.element, 'Sort', ['button-primary']);
+
+  toyCards: ToyCards;
+
+  constructor(items: IToyProps[]) {
     super(['toys-page']);
+
+    this.toyCards = new ToyCards(this.element, items);
   }
 
   async render(): Promise<void> {
-    this.content.element.innerHTML = `
-      <h2>Toys Page</h2>
-    `;
-
-    this.element.append(this.content.element);
+    this.filterButton.render();
+    this.sortButton.render();
+    this.toyCards.render();
   }
 
-  // eslint-disable-next-line class-methods-use-this
   async afterRender(): Promise<void> {
-    //
+    this.filterButton.element.onclick = () => {
+      this.toyCards.cards.forEach((card) => {
+        if (card.props.year > 1980) {
+          card.element.style.display = 'none';
+        }
+      });
+    };
+
+    this.sortButton.element.onclick = () => {
+      this.toyCards.cards.sort((a, b) => {
+        if (a.props.name > b.props.name) return 1;
+        if (a.props.name < b.props.name) return -1;
+        return 0;
+      });
+
+      this.toyCards.cards.forEach((card, index) => {
+        card.element.style.order = index.toString();
+      });
+    };
   }
 }
 
