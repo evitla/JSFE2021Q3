27c3c17fe3e991550e033fc176ff73aed02909b8
evitla <s.dinmuhamed@gmail.com>
2021-12-15 15:28:27 +0600

    feat: add toy props type

---
+++ christmas-task/src/types/index.ts
@@ -0,0 +1,11 @@
+export type IToyProps = {
+  num: number;
+  name: string;
+  image: string;
+  count: number;
+  year: number;
+  shape: string;
+  color: string;
+  size: string;
+  favorite: boolean;
+};
