50fcee15ac9edfd6c9faf7904b9c355e24b4d695
evitla <s.dinmuhamed@gmail.com>
2021-12-28 19:08:56 +0600

    feat: render 20 toys if no favorite toys

--- christmas-task/src/pages/Tree/index.ts
+++ christmas-task/src/pages/Tree/index.ts
@@ -1,22 +1,25 @@
-import BaseComponent from '../../components/BaseComponent';
 import BasePage from '../BasePage';
 import ToyCard from '../../components/ToyCards/toyCard';
-import { PageClassNameType } from '../../types';
 
 import './style.scss';
+
+const MAX_TOYS = 20;
 
 class Tree extends BasePage {
   favoriteCards: ToyCard[];
 
-  constructor(name: PageClassNameType) {
-    super(name);
-  }
-
   async render(toyCards: ToyCard[]): Promise<void> {
+    toyCards.forEach((card) => card.render());
+
     const favoriteCards = toyCards.filter(
       (card) => card.props.personalFavorite
     );
-    this.element.append(...favoriteCards.map((c) => c.element));
+    this.element.append(
+      ...(favoriteCards.length !== 0
+        ? favoriteCards
+        : toyCards.slice(MAX_TOYS)
+      ).map((c) => c.element)
+    );
   }
 
   // eslint-disable-next-line class-methods-use-this
