b509cd49ef749ba19f1dcc7bab67db847976d325
evitla <s.dinmuhamed@gmail.com>
2021-12-28 20:03:28 +0600

    fix: toy cards slice

--- christmas-task/src/pages/Tree/index.ts
+++ christmas-task/src/pages/Tree/index.ts
@@ -1,4 +1,5 @@
 import BasePage from '../BasePage';
+import BaseComponent from '../../components/BaseComponent';
 import ToyCard from '../../components/ToyCards/toyCard';
 
 import './style.scss';
@@ -8,18 +9,23 @@ const MAX_TOYS = 20;
 class Tree extends BasePage {
   favoriteCards: ToyCard[];
 
+  toys = new BaseComponent('div', ['toys']);
+
   async render(toyCards: ToyCard[]): Promise<void> {
-    toyCards.forEach((card) => card.render());
+    const cards = toyCards.slice(0, MAX_TOYS);
+
+    cards.forEach((card) => card.render(true));
 
     const favoriteCards = toyCards.filter(
       (card) => card.props.personalFavorite
     );
-    this.element.append(
-      ...(favoriteCards.length !== 0
-        ? favoriteCards
-        : toyCards.slice(MAX_TOYS)
-      ).map((c) => c.element)
+    this.toys.element.append(
+      ...(favoriteCards.length !== 0 ? favoriteCards : cards).map(
+        (c) => c.element
+      )
     );
+
+    this.element.appendChild(this.toys.element);
   }
 
   // eslint-disable-next-line class-methods-use-this
