a1be884e7d4a4c11f5d12a8000896fed94c3c2a9
evitla <s.dinmuhamed@gmail.com>
2021-12-29 12:20:31 +0600

    feat: add Trees and MainTree to Tree page

--- christmas-task/src/pages/Tree/index.ts
+++ christmas-task/src/pages/Tree/index.ts
@@ -1,6 +1,8 @@
 import BasePage from '../BasePage';
 import BaseComponent from '../../components/BaseComponent';
 import ToyCard from '../../components/ToyCards/toyCard';
+import Trees from '../../components/Trees';
+import { PageClassNameType } from '../../types';
 
 import './style.scss';
 
@@ -9,9 +11,38 @@ const MAX_TOYS = 20;
 class Tree extends BasePage {
   favoriteCards: ToyCard[];
 
+  leftSide = new BaseComponent('div', ['left-side']);
+
+  mainTree = new BaseComponent('div', ['main-tree']);
+
+  mainTreeImage = new Image();
+
+  treesContainer = new Trees(this.leftSide.element);
+
   toys = new BaseComponent('div', ['toys']);
 
+  constructor(pageName: PageClassNameType) {
+    super(pageName);
+
+    this.mainTreeImage.src = this.treesContainer.trees[0].src;
+    this.mainTreeImage.alt = 'main-tree';
+  }
+
   async render(toyCards: ToyCard[]): Promise<void> {
+    this.treesContainer.render();
+    this.mainTree.element.appendChild(this.mainTreeImage);
+    this.element.appendChild(this.leftSide.element);
+    this.element.appendChild(this.mainTree.element);
+    this.renderFavoriteToys(toyCards);
+  }
+
+  async afterRender(): Promise<void> {
+    this.treesContainer.afterRender((target: HTMLImageElement) => {
+      this.mainTreeImage.src = target.src;
+    });
+  }
+
+  private renderFavoriteToys(toyCards: ToyCard[]) {
     const cards = toyCards.slice(0, MAX_TOYS);
 
     cards.forEach((card) => card.render(true));
@@ -24,13 +55,7 @@ class Tree extends BasePage {
         (c) => c.element
       )
     );
-
     this.element.appendChild(this.toys.element);
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  async afterRender(): Promise<void> {
-    //
   }
 }
 
