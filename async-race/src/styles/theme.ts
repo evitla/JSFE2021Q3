2d21d39876597e5f5a22eac6fe6803a94ad3d247
evitla <s.dinmuhamed@gmail.com>
2021-12-30 17:12:12 +0600

    feat: add base theme

---
+++ async-race/src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { ITheme } from '../interfaces/styled';
+
+export const baseTheme: ITheme = {
+  fontFamily: `'Unica One', cursive`,
+
+  colors: {
+    bg: '#070417',
+    font: 'white',
+  },
+};
