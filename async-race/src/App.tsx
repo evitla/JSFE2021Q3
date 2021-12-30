4d771fa1467ffd96d225adc0e922c0ee9b2f9923
evitla <s.dinmuhamed@gmail.com>
2021-12-31 00:50:07 +0600

    feat: add Routes

--- async-race/src/App.tsx
+++ async-race/src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
 import { ThemeProvider } from 'styled-components';
 
 import Header from './components/Header';
+import { Race, Winners } from './pages';
+import Wrapper from './styles/components/Wrapper';
 
 import GlobalStyles from './styles/global';
 import { baseTheme } from './styles/theme';
@@ -11,6 +14,14 @@ function App() {
     <div className="App">
       <ThemeProvider theme={baseTheme}>
         <Header />
+        <main>
+          <Wrapper>
+            <Routes>
+              <Route path="/" element={<Race />} />
+              <Route path="/winners" element={<Winners />} />
+            </Routes>
+          </Wrapper>
+        </main>
         <GlobalStyles />
       </ThemeProvider>
     </div>
