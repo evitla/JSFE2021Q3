466ff5f3bf43a9d138e4f2f779c5d45bd2dabb34
evitla <s.dinmuhamed@gmail.com>
2021-12-31 01:40:02 +0600

    feat: add colors

--- async-race/src/interfaces/styled.ts
+++ async-race/src/interfaces/styled.ts
@@ -2,6 +2,14 @@ export interface ITheme {
   fontFamily: string;
 
   colors: {
+    primary: {
+      100: string;
+      110: string;
+      120: string;
+      130: string;
+      140: string;
+    };
+
     bg: string;
     font: string;
   };
--- async-race/src/styles/theme.ts
+++ async-race/src/styles/theme.ts
@@ -4,6 +4,14 @@ export const baseTheme: ITheme = {
   fontFamily: `'Unica One', cursive`,
 
   colors: {
+    primary: {
+      100: 'hsl(48deg, 100%, 84%)',
+      110: 'hsl(48deg, 100%, 76%)',
+      120: 'hsl(48deg, 100%, 68%)',
+      130: 'hsl(48deg, 100%, 60%)',
+      140: 'hsl(48deg, 100%, 36%)',
+    },
+
     bg: '#070417',
     font: 'white',
   },
