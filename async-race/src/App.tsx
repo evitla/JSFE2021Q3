c9a2e72988ee3320780362d614a682646b3cad49
evitla <s.dinmuhamed@gmail.com>
2021-12-30 14:58:26 +0600

    init: start async-race task

---
+++ async-race/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import logo from './assets/images/logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <h1>Hello, World!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
