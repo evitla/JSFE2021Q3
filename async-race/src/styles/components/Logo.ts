9800a43f0d547e6ba6105cc968877c9f4dbeece3
evitla <s.dinmuhamed@gmail.com>
2021-12-30 16:41:17 +0600

    feat: add Logo styled component

---
+++ async-race/src/styles/components/Logo.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 0.75rem;
+
+  img {
+    width: 100px;
+  }
+
+  h1 {
+    font-size: 1.625rem;
+  }
+`;
+
+export default Logo;
