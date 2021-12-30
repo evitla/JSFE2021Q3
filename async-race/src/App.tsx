ce081bb1b8bdee87b2bcd897ae2e4cba2b5e7b5e
evitla <s.dinmuhamed@gmail.com>
2021-12-30 17:13:34 +0600

    feat: add global style

--- async-race/src/App.tsx
+++ async-race/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import Header from './components/Header';
+
+import GlobalStyles from './styles/global';
+import { baseTheme } from './styles/theme';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <ThemeProvider theme={baseTheme}>
+        <Header />
+        <GlobalStyles />
+      </ThemeProvider>
     </div>
   );
 }
