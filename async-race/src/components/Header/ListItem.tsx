20dca6bd1b1ba3c9db344f0d506e194101d0d2c3
evitla <s.dinmuhamed@gmail.com>
2021-12-31 01:22:00 +0600

    refactor: change ListItem active with useLocation

--- async-race/src/components/Header/ListItem.tsx
+++ async-race/src/components/Header/ListItem.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import { NavLinks } from '../../constants';
 import { StyledListItem } from '../../styles/components';
 
-const ListItem = ({ item, active, onSelect }: ListProps) => {
+const ListItem = ({ item }: { item: string }) => {
+  const { pathname } = useLocation();
+
+  const isActive = pathname === NavLinks[item];
+
   return (
-    <StyledListItem active={active} onClick={onSelect}>
+    <StyledListItem active={isActive}>
       <Link to={NavLinks[item]}>{item}</Link>
     </StyledListItem>
   );
 };
 
 export default ListItem;
-
-type ListProps = {
-  item: string;
-  active: boolean;
-  onSelect: () => void;
-};
--- async-race/src/components/Header/Navbar.tsx
+++ async-race/src/components/Header/Navbar.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
 import ListItem from './ListItem';
 
-const Navbar = ({ items }: { items: string[] }) => {
-  const [activeItem, setActiveItem] = React.useState('Race');
-
-  const handleSelect = (item: string) => {
-    setActiveItem(item);
-  };
-
-  return (
-    <nav>
-      <ul>
-        {items.map((item) => (
-          <ListItem
-            key={item}
-            item={item}
-            active={item === activeItem}
-            onSelect={() => handleSelect(item)}
-          />
-        ))}
-      </ul>
-    </nav>
-  );
-};
+const Navbar = ({ items }: { items: string[] }) => (
+  <nav>
+    <ul>
+      {items.map((item) => (
+        <ListItem key={item} item={item} />
+      ))}
+    </ul>
+  </nav>
+);
 
 export default Navbar;
