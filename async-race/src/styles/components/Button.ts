2a4e52c77ed0e7c803f83722a55c823e528ef26f
evitla <s.dinmuhamed@gmail.com>
2021-12-31 01:40:24 +0600

    feat: add Button styled component

---
+++ async-race/src/styles/components/Button.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  display: inline-block;
+  appearance: none;
+  margin: 0 auto;
+  border: none;
+  font-size: 1.25rem;
+  font-weight: bold;
+  padding: 0.5em 1em;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.14s;
+  outline: none;
+  text-align: center;
+  width: 100%;
+  font-family: inherit;
+
+  &:active {
+    transform: translateY(3px);
+  }
+
+  &.button-primary {
+    box-shadow: 0 3px 0 0 ${({ theme }) => theme.colors.primary[140]};
+    background: ${({ theme }) => theme.colors.primary[130]};
+    color: ${({ theme }) => theme.colors.bg};
+
+    &:hover {
+      background: ${({ theme }) => theme.colors.primary[110]};
+    }
+
+    &:active {
+      box-shadow: 0 0 0 0 ${({ theme }) => theme.colors.primary[140]};
+    }
+  }
+`;
+
+export default Button;
--- async-race/src/styles/components/index.ts
+++ async-race/src/styles/components/index.ts
@@ -1,2 +1,4 @@
-export { default as Wrapper } from './Wrapper';
+export { default as HeaderWrapper } from './HeaderWrapper';
+export { default as StyledListItem } from './ListItem';
 export { default as StyledLogo } from './Logo';
+export { default as StyledButton } from './Button';
