79af58bbe2e85a51c95ba2bb2149d11f1ead529a
evitla <s.dinmuhamed@gmail.com>
2022-01-03 11:50:02 +0600

    feat: add 3 tracks manually

--- async-race/src/pages/Race/index.tsx
+++ async-race/src/pages/Race/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import CarForm from '../../components/Race/CarForm';
+import Track from '../../components/Track';
 
 const Race = () => (
   <>
     <CarForm content="Create" />
     <CarForm content="Update" isDisabled={true} />
+    <Track carColor="green" />
+    <Track carColor="white" />
+    <Track carColor="red" />
   </>
 );
 
