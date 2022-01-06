b0cd2a9ffb903a61cf5b716531d86a38d9c5827e
evitla <s.dinmuhamed@gmail.com>
2022-01-06 23:16:06 +0600

    feat: handle select and update car

--- async-race/src/pages/Race/index.tsx
+++ async-race/src/pages/Race/index.tsx
@@ -2,15 +2,62 @@ import React from 'react';
 
 import CarForm from '../../components/Race/CarForm';
 import Track from '../../components/Track';
+import { ICarProps } from '../../interfaces/CarProps';
 
-const Race = () => (
-  <>
-    <CarForm content="Create" />
-    <CarForm content="Update" isDisabled={true} />
-    <Track carColor="green" />
-    <Track carColor="white" />
-    <Track carColor="red" />
-  </>
-);
+const carsData: ICarProps[] = [
+  { id: 1, model: 'BMW', color: 'green' },
+  { id: 2, model: 'Ford', color: 'white' },
+  { id: 3, model: 'Kia', color: 'red' },
+];
+
+const Race = () => {
+  const [cars, setCars] = React.useState(carsData);
+  const [selectedCar, setSelectedCar] = React.useState<ICarProps | null>(null);
+
+  const handleUpdate = (model: string, color: string) => {
+    const selectedCarIndex = cars.findIndex(
+      (car) => car.id === selectedCar?.id,
+    );
+
+    if (selectedCarIndex !== -1) {
+      const updatedCar = cars[selectedCarIndex];
+      updatedCar.model = model;
+      updatedCar.color = color;
+
+      setCars([
+        ...cars.slice(0, selectedCarIndex),
+        updatedCar,
+        ...cars.slice(selectedCarIndex + 1),
+      ]);
+
+      setSelectedCar(null);
+    }
+  };
+
+  const handleSelectCar = (id: number) => {
+    const selected = cars.find((car) => car.id === id);
+    if (selected !== undefined) {
+      setSelectedCar(selected);
+    }
+  };
+
+  return (
+    <>
+      <CarForm content="Create" />
+      <CarForm
+        content="Update"
+        isDisabled={selectedCar === null}
+        onUpdateCar={handleUpdate}
+      />
+      {cars.map((car) => (
+        <Track
+          key={car.id}
+          carProps={car}
+          onSelectCar={() => handleSelectCar(car.id)}
+        />
+      ))}
+    </>
+  );
+};
 
 export default Race;
