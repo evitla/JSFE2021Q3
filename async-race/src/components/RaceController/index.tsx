c98ab1c5c0943880837bf31218a73ac473a0f0c9
evitla <s.dinmuhamed@gmail.com>
2022-01-13 16:58:14 +0600

    feat: add Generate Cars button

--- async-race/src/components/RaceController/index.tsx
+++ async-race/src/components/RaceController/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { ICarProps } from '../../interfaces/CarProps';
 import { onCreateCar, onUpdateCar } from '../../slices/race';
+import { StyledButton, StyledRaceController } from '../../styles/components';
 import { createCar, updateCar } from '../../utils';
 import CarForm from './CarForm';
 
@@ -22,14 +23,15 @@ const RaceController = ({ selectedCar }: { selectedCar: ICarProps | null }) => {
   };
 
   return (
-    <>
+    <StyledRaceController>
       <CarForm content="Create" onCreateCar={handleCreate} />
       <CarForm
         content="Update"
         isDisabled={selectedCar === null}
         onUpdateCar={handleUpdate}
       />
-    </>
+      <StyledButton className="button-primary">Generate Cars</StyledButton>
+    </StyledRaceController>
   );
 };
 
