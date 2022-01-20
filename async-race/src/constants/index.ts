869bace4d5ee1b2c20b165f915685702182c870a
evitla <s.dinmuhamed@gmail.com>
2022-01-20 20:20:10 +0600

    feat: add engine url

--- async-race/src/constants/index.ts
+++ async-race/src/constants/index.ts
@@ -3,7 +3,9 @@ export const NavLinks: { [key: string]: string } = {
   Winners: '/winners',
 };
 
-export const URL = 'http://localhost:3000/garage';
+const BASE_URL = 'http://localhost:3000';
+export const GARAGE_URL = `${BASE_URL}/garage`;
+export const ENGINE_URL = `${BASE_URL}/engine`;
 
 export const GENERATE_CARS_COUNT = 100;
 
