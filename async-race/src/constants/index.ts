2ddf573a6a47ca063fa4b76e8903e5d96739053f
evitla <s.dinmuhamed@gmail.com>
2022-01-20 20:18:37 +0600

    feat: add constants to generate cars

--- async-race/src/constants/index.ts
+++ async-race/src/constants/index.ts
@@ -4,3 +4,32 @@ export const NavLinks: { [key: string]: string } = {
 };
 
 export const URL = 'http://localhost:3000/garage';
+
+export const GENERATE_CARS_COUNT = 100;
+
+export const carModels = [
+  'Tesla',
+  'Mercedes',
+  'BMW',
+  'Ferrari',
+  'Honda',
+  'Toyota',
+  'Ford',
+  'Volkswagen',
+  'Porsche',
+  'Bentley',
+];
+
+export const carSeries = [
+  'Model S',
+  'Camry',
+  'CLK',
+  'Combi',
+  'Cayene',
+  '7 Series',
+  '9 Series',
+  '11 Series',
+  'DB9',
+  'Model E',
+  'X5',
+];
