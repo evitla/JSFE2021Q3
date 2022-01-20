cefe60a901e264c904eb75d05d8fba4b7dcf6d5a
evitla <s.dinmuhamed@gmail.com>
2022-01-20 20:25:25 +0600

    feat: add startDriving and stopDriving methods

--- async-race/src/components/Car/index.tsx
+++ async-race/src/components/Car/index.tsx
@@ -8,10 +8,34 @@ import { ICarProps } from '../../interfaces/CarProps';
 
 import { StyledCar } from '../../styles/components';
 import { onDeleteCar, onGetCar } from '../../slices/race';
-import { deleteCar } from '../../utils';
-import { URL } from '../../constants';
+import {
+  animate,
+  deleteCar,
+  drive,
+  startEngine,
+  stopEngine,
+} from '../../utils';
+import { ENGINE_URL, GARAGE_URL } from '../../constants';
 
-const Car = ({ carProps }: { carProps: ICarProps }) => {
+const CAR_INITIAL_POSITION = 0;
+const CAR_WIDTH = 100;
+
+const Car = ({
+  carProps,
+  trackLength,
+}: {
+  carProps: ICarProps;
+  trackLength: number;
+}) => {
+  const [carContainer, setCarContainer] = React.useState<HTMLElement | null>(
+    null
+  );
+  const carRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    if (carRef.current) setCarContainer(carRef.current);
+  }, []);
+
   const dispatch = useDispatch();
 
   const handleSelect = (id: number) => {
@@ -19,14 +43,46 @@ const Car = ({ carProps }: { carProps: ICarProps }) => {
   };
 
   const handleRemove = async (id: number) => {
-    await deleteCar(URL, id);
+    await deleteCar(GARAGE_URL, id);
     dispatch(onDeleteCar(id));
   };
 
+  let animationState: { id: number | null } = { id: null };
+
+  const startDriving = async () => {
+    const { velocity, distance } = await startEngine(ENGINE_URL, carProps.id);
+    const duration = Math.round(distance / velocity);
+    const htmlDistance = trackLength - CAR_WIDTH;
+
+    animationState = animate(htmlDistance, duration, (progress) => {
+      if (carContainer !== null) {
+        carContainer.style.transform = `translateX(${progress}px)`;
+      }
+    });
+
+    const { success } = await drive(ENGINE_URL, carProps.id);
+
+    if (!success && animationState.id !== null) {
+      window.cancelAnimationFrame(animationState.id);
+    }
+  };
+
+  const stopDriving = async () => {
+    await stopEngine(ENGINE_URL, carProps.id);
+
+    if (carContainer !== null) {
+      carContainer.style.transform = `translateX(${CAR_INITIAL_POSITION}px)`;
+    }
+
+    if (animationState.id !== null) {
+      window.cancelAnimationFrame(animationState.id);
+    }
+  };
+
   return (
-    <StyledCar>
-      <CarController />
-      <figure>
+    <StyledCar carWidth={CAR_WIDTH}>
+      <CarController onStart={startDriving} onStop={stopDriving} />
+      <figure ref={carRef}>
         <CarIcon color={carProps.color} />
         <CarTooltip
           onSelect={() => handleSelect(carProps.id)}
