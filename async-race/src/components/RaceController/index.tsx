584942cb9f7b425cf9d1f55adf5154dce2665d3b
evitla <s.dinmuhamed@gmail.com>
2022-01-20 20:20:52 +0600

    feat: generate random cars

--- async-race/src/components/RaceController/index.tsx
+++ async-race/src/components/RaceController/index.tsx
@@ -3,23 +3,35 @@ import { useDispatch } from 'react-redux';
 import { ICarProps } from '../../interfaces/CarProps';
 import { onCreateCar, onUpdateCar } from '../../slices/race';
 import { StyledButton, StyledRaceController } from '../../styles/components';
-import { createCar, updateCar } from '../../utils';
+import { createCar, generateCars, updateCar } from '../../utils';
 import CarForm from './CarForm';
-import { URL } from '../../constants';
+import { GENERATE_CARS_COUNT, GARAGE_URL } from '../../constants';
 
 const RaceController = ({ selectedCar }: { selectedCar: ICarProps | null }) => {
   const dispatch = useDispatch();
 
   const handleCreate = async (name: string, color: string) => {
-    const newCar = await createCar(URL, { name, color });
+    const newCar = await createCar(GARAGE_URL, { name, color });
     dispatch(onCreateCar(newCar));
   };
 
   const handleUpdate = async (name: string, color: string) => {
     if (selectedCar !== null) {
-      const newCar = await updateCar(URL, { ...selectedCar, name, color });
+      const newCar = await updateCar(GARAGE_URL, {
+        ...selectedCar,
+        name,
+        color,
+      });
       dispatch(onUpdateCar(newCar));
     }
+  };
+
+  const handleGenerateCars = async () => {
+    const cars = generateCars(GENERATE_CARS_COUNT);
+    const newCars = await Promise.all(
+      cars.map((car) => createCar(GARAGE_URL, car))
+    );
+    newCars.forEach((car) => dispatch(onCreateCar(car)));
   };
 
   return (
@@ -30,7 +42,9 @@ const RaceController = ({ selectedCar }: { selectedCar: ICarProps | null }) => {
         isDisabled={selectedCar === null}
         onUpdateCar={handleUpdate}
       />
-      <StyledButton className="button-primary">Generate Cars</StyledButton>
+      <StyledButton className="button-primary" onClick={handleGenerateCars}>
+        Generate Cars
+      </StyledButton>
     </StyledRaceController>
   );
 };
--- async-race/src/utils/index.ts
+++ async-race/src/utils/index.ts
@@ -1,3 +1,4 @@
+import { carModels, carSeries } from '../constants';
 import { ICarProps, ICarPropsToCreate } from '../interfaces/CarProps';
 
 export const getCars = async (url: string): Promise<ICarProps[]> => {
@@ -41,4 +42,29 @@ export const deleteCar = async (url: string, id: number) => {
   const response = await fetch(`${url}/${id}`, { method: 'DELETE' });
   const car = await response.json();
   return car;
+};
+
+const getRandomNumber = (max: number) => Math.floor(Math.random() * max);
+
+const getRandomCarName = () => {
+  const model = carModels[getRandomNumber(carModels.length)];
+  const series = carSeries[getRandomNumber(carSeries.length)];
+  return `${model} ${series}`;
+};
+
+const getRandomColor = () => {
+  const chars = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += chars[getRandomNumber(chars.length)];
+  }
+
+  return color;
+};
+
+export const generateCars = (count: number): ICarPropsToCreate[] => {
+  return Array.from({ length: count }, () => ({
+    name: getRandomCarName(),
+    color: getRandomColor(),
+  }));
 };
