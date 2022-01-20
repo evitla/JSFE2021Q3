0080818e75b975c36d0f271d7115518fe17832c9
evitla <s.dinmuhamed@gmail.com>
2022-01-20 20:24:45 +0600

    feat: get track length

--- async-race/src/components/Track/index.tsx
+++ async-race/src/components/Track/index.tsx
@@ -3,10 +3,21 @@ import Car from '../Car';
 import { ICarProps } from '../../interfaces/CarProps';
 import { StyledTrack } from '../../styles/components';
 
-const Track = ({ carProps }: { carProps: ICarProps }) => (
-  <StyledTrack>
-    <Car carProps={carProps} />
-  </StyledTrack>
-);
+const DEFAULT_TRACK_LENGTH = 1000;
+
+const Track = ({ carProps }: { carProps: ICarProps }) => {
+  const [length, setLength] = React.useState(DEFAULT_TRACK_LENGTH);
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    if (ref.current) setLength(ref.current.offsetWidth);
+  }, []);
+
+  return (
+    <StyledTrack ref={ref}>
+      <Car carProps={carProps} trackLength={length} />
+    </StyledTrack>
+  );
+};
 
 export default Track;
