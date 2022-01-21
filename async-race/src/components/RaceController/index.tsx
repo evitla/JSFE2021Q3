9e3041496db061a6e9bd2563da3f92d8b506c0ef
evitla <s.dinmuhamed@gmail.com>
2022-01-21 23:43:04 +0600

    feat: add Start Race button

--- async-race/src/components/RaceController/index.tsx
+++ async-race/src/components/RaceController/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { ICarProps } from '../../interfaces/CarProps';
-import { onCreateCar, onUpdateCar } from '../../slices/race';
+import { onCreateCar, onStartEngine, onUpdateCar } from '../../slices/race';
 import { StyledButton, StyledRaceController } from '../../styles/components';
-import { createCar, generateCars, updateCar } from '../../utils';
+import { createCar, generateCars, startEngine, updateCar } from '../../utils';
 import CarForm from './CarForm';
-import { GENERATE_CARS_COUNT, GARAGE_URL } from '../../constants';
+import { GENERATE_CARS_COUNT, GARAGE_URL, ENGINE_URL } from '../../constants';
 
-const RaceController = ({ selectedCar }: { selectedCar: ICarProps | null }) => {
+const RaceController = ({
+  cars,
+  selectedCar,
+}: {
+  cars: ICarProps[];
+  selectedCar: ICarProps | null;
+}) => {
   const dispatch = useDispatch();
 
   const handleCreate = async (name: string, color: string) => {
@@ -27,11 +33,20 @@ const RaceController = ({ selectedCar }: { selectedCar: ICarProps | null }) => {
   };
 
   const handleGenerateCars = async () => {
-    const cars = generateCars(GENERATE_CARS_COUNT);
+    const generatedCars = generateCars(GENERATE_CARS_COUNT);
     const newCars = await Promise.all(
-      cars.map((car) => createCar(GARAGE_URL, car))
+      generatedCars.map((car) => createCar(GARAGE_URL, car))
     );
     newCars.forEach((car) => dispatch(onCreateCar(car)));
+  };
+
+  const handleStartRace = async () => {
+    await Promise.all(
+      cars.map(async (car) => {
+        const { velocity, distance } = await startEngine(ENGINE_URL, car.id);
+        dispatch(onStartEngine({ id: car.id, velocity, distance }));
+      })
+    );
   };
 
   return (
@@ -42,9 +57,14 @@ const RaceController = ({ selectedCar }: { selectedCar: ICarProps | null }) => {
         isDisabled={selectedCar === null}
         onUpdateCar={handleUpdate}
       />
-      <StyledButton className="button-primary" onClick={handleGenerateCars}>
-        Generate Cars
-      </StyledButton>
+      <div style={{ display: 'flex' }}>
+        <StyledButton className="button-primary" onClick={handleStartRace}>
+          Start Race
+        </StyledButton>
+        <StyledButton className="button-primary" onClick={handleGenerateCars}>
+          Generate Cars
+        </StyledButton>
+      </div>
     </StyledRaceController>
   );
 };
