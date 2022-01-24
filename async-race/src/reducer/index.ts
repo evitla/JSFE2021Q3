086e1c24490d9fe2c1b5621c4eee9296def267cf
evitla <s.dinmuhamed@gmail.com>
2022-01-24 23:49:31 +0600

    feat: add winner reducer

--- async-race/src/reducer/index.ts
+++ async-race/src/reducer/index.ts
@@ -1,7 +1,9 @@
 import { raceReducer } from '../slices/race';
+import { winnerReducer } from '../slices/winner';
 
 const reducer = {
   raceReducer,
+  winnerReducer,
 };
 
 export default reducer;
