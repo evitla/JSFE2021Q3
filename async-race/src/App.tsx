b45e6888704dd3c5000d826a0694ca00252647b1
evitla <s.dinmuhamed@gmail.com>
2022-01-25 12:10:09 +0600

    feat: call getCars function when racePage changes

--- async-race/src/App.tsx
+++ async-race/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 
 import Header from './components/Header';
@@ -12,15 +12,18 @@ import { CARS_PER_PAGE, GARAGE_URL } from './constants';
 import GlobalStyles from './styles/global';
 import { baseTheme } from './styles/theme';
 import Wrapper from './styles/components/Wrapper';
+import { TStore } from './store';
 
 function App() {
   const dispatch = useDispatch();
 
+  const { racePage } = useSelector((state: TStore) => state.raceReducer);
+
   React.useEffect(() => {
-    getCars(`${GARAGE_URL}?_page=${1}&_limit=${CARS_PER_PAGE}`).then((data) =>
-      dispatch(onSaveCars(data))
-    );
-  }, [dispatch]);
+    getCars(GARAGE_URL, racePage, CARS_PER_PAGE).then((data) => {
+      dispatch(onSaveCars({ ...data }));
+    });
+  }, [racePage]);
 
   return (
     <div className="App">
