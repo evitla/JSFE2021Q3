f5a6fd8476f6752ba549f1865913a9d8d46133e8
evitla <s.dinmuhamed@gmail.com>
2022-01-26 13:13:38 +0600

    feat: call getWinners when winnersPage, sortBy or sortOrder changes

--- async-race/src/pages/Winners/index.tsx
+++ async-race/src/pages/Winners/index.tsx
@@ -1,27 +1,47 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import Pagination from '../../components/Pagination';
 import WinnersTable from '../../components/WinnersTable';
-import { GARAGE_URL, WINNERS_URL } from '../../constants';
-import { onGetWinners } from '../../slices/winner';
+import { GARAGE_URL, WINNERS_PER_PAGE, WINNERS_URL } from '../../constants';
+import { onGetWinners, onNextPage, onPrevPage } from '../../slices/winner';
+import { TStore } from '../../store';
 import { getWinners } from '../../utils';
 
 const Winners = () => {
   const [winnersCount, setWinnersCount] = React.useState(0);
 
-  const dispath = useDispatch();
+  const dispatch = useDispatch();
 
-  getWinners(WINNERS_URL, GARAGE_URL, 1, 7, 'id', 'ASC').then((data) => {
-    if (data.count !== null) {
-      setWinnersCount(+data.count);
-    }
+  const { winnersPage, sortBy, sortOrder } = useSelector(
+    (state: TStore) => state.winnerReducer
+  );
 
-    dispath(onGetWinners(data.winners));
-  });
+  React.useEffect(() => {
+    getWinners(
+      WINNERS_URL,
+      GARAGE_URL,
+      winnersPage,
+      WINNERS_PER_PAGE,
+      sortBy,
+      sortOrder
+    ).then((data) => {
+      if (data.count !== null) {
+        setWinnersCount(+data.count);
+      }
+
+      dispatch(onGetWinners(data.winners));
+    });
+  }, [winnersPage, sortBy, sortOrder]);
 
   return (
     <>
       <h2>{`Winners(${winnersCount})`}</h2>
+      <h3>{`Page ${winnersPage}`}</h3>
       <WinnersTable />
+      <Pagination
+        onPrev={() => dispatch(onPrevPage())}
+        onNext={() => dispatch(onNextPage())}
+      />
     </>
   );
 };
