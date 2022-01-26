ba71de8f5ce044a513e7a066ebcf4f853e2913f0
evitla <s.dinmuhamed@gmail.com>
2022-01-26 13:14:38 +0600

    feat: add onPrevPage, onNextPage, onSort actions

--- async-race/src/slices/winner.ts
+++ async-race/src/slices/winner.ts
@@ -1,10 +1,17 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IWinnerProps } from '../interfaces/WinnerProps';
+import { TSortBy, TSortOrder } from '../utils';
 
 const initialState: {
   winners: IWinnerProps[];
+  winnersPage: number;
+  sortBy: TSortBy;
+  sortOrder: TSortOrder;
 } = {
   winners: [],
+  winnersPage: 1,
+  sortBy: 'id',
+  sortOrder: 'ASC',
 };
 
 const winnerSlice = createSlice({
@@ -14,8 +21,25 @@ const winnerSlice = createSlice({
     onGetWinners: (state, { payload }: PayloadAction<IWinnerProps[]>) => {
       state.winners = payload;
     },
+
+    onNextPage: (state) => {
+      state.winnersPage++;
+    },
+
+    onPrevPage: (state) => {
+      state.winnersPage--;
+    },
+
+    onSort: (
+      state,
+      { payload }: PayloadAction<{ sortBy: TSortBy; sortOrder: TSortOrder }>
+    ) => {
+      state.sortBy = payload.sortBy;
+      state.sortOrder = payload.sortOrder;
+    },
   },
 });
 
 export const winnerReducer = winnerSlice.reducer;
-export const { onGetWinners } = winnerSlice.actions;
+export const { onGetWinners, onPrevPage, onNextPage, onSort } =
+  winnerSlice.actions;
