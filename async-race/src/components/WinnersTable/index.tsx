059252b57064ada891de8ddc2cce85ca2b9ef8e8
evitla <s.dinmuhamed@gmail.com>
2022-01-26 13:14:57 +0600

    feat: handle sorting

--- async-race/src/components/WinnersTable/index.tsx
+++ async-race/src/components/WinnersTable/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { TStore } from '../../store';
 import { StyledWinnersTable } from '../../styles/components';
 import CarIcon from '../Car/CarIcon';
 
 import chevron from '../../assets/images/chevron.svg';
+import { TSortBy, TSortOrder } from '../../utils';
+import { onSort } from '../../slices/winner';
 
 const WinnersTable = () => {
   const { winners } = useSelector((state: TStore) => state.winnerReducer);
+
+  const dispatch = useDispatch();
+
+  const [sortOrder, setSortOrder] = React.useState<TSortOrder>('ASC');
+
+  const handleSort = (sortBy: TSortBy) => {
+    dispatch(onSort({ sortBy, sortOrder }));
+    setSortOrder((value) => (value === 'ASC' ? 'DESC' : 'ASC'));
+  };
 
   return (
     <StyledWinnersTable>
@@ -16,11 +27,11 @@ const WinnersTable = () => {
           <th>Number</th>
           <th>Car</th>
           <th>Name</th>
-          <th className="wins-column">
+          <th className="wins-column" onClick={() => handleSort('wins')}>
             <span>Wins</span>
             <img src={chevron} alt="" />
           </th>
-          <th className="best-time-column">
+          <th className="best-time-column" onClick={() => handleSort('time')}>
             <span>Best time (seconds)</span>
             <img src={chevron} alt="" />
           </th>
